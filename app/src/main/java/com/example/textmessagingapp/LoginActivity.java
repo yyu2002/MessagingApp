afa57cbc063cc74c77d4119b188297a302b85c09
Young Yu <youngyu2002@gmail.com>
2021-10-11 16:13:28 -0400

    linked the login and create new account pages, defined and initialized the input fields and button fields on Login and Register Activity pages

--- app/src/main/java/com/example/textmessagingapp/LoginActivity.java
+++ app/src/main/java/com/example/textmessagingapp/LoginActivity.java
@@ -5,17 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser; // current user
+    private Button LoginButton; // login button
+    private Button PhoneLoginButton; // login with phone code button
+    private EditText UserEmail; // email field
+    private EditText UserPassword; // password field
+    private TextView NeedNewAccountLink; // need new account link
+    private TextView ForgotPasswordLink; // forgot password link
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        InitializeFields(); // initialize class fields
+
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendUserToRegisterActivity();
+            }
+        }); // when user clicks on link, send to register activity
+    }
+
+    private void InitializeFields() {
+        LoginButton = (Button) findViewById(R.id.login_button);
+        PhoneLoginButton = (Button) findViewById(R.id.phone_login_button);
+        UserEmail = (EditText) findViewById(R.id.login_email);
+        UserPassword = (EditText) findViewById(R.id.login_password);
+        NeedNewAccountLink = (TextView) findViewById(R.id.need_new_account_link);
+        ForgotPasswordLink = (TextView) findViewById(R.id.forgot_password_link);
+
     }
 
     @Override
@@ -31,5 +61,10 @@ public class LoginActivity extends AppCompatActivity {
     private void sendUserToMainActivity() {
         Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(loginIntent); // send user to main activity if logged in already
+    }
+
+    private void sendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent); // send user to main activity if logged in already
     }
 }
--- app/src/main/java/com/example/textmessagingapp/RegisterActivity.java
+++ app/src/main/java/com/example/textmessagingapp/RegisterActivity.java
@@ -2,13 +2,45 @@ package com.example.textmessagingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button CreateAccountButton; // create account button
+    private EditText UserEmail; // email field
+    private EditText UserPassword; // password field
+    private TextView AlreadyHaveAccountLink; // already have account link
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        InitializeFields(); // initialize class fields
+
+        AlreadyHaveAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendUserToLoginActivity();
+            }
+        }); // when user clicks on link, send to login activity
+
+    }
+
+    private void InitializeFields() {
+        CreateAccountButton = (Button) findViewById(R.id.register_button);
+        UserEmail = (EditText) findViewById(R.id.register_email);
+        UserPassword = (EditText) findViewById(R.id.register_password);
+        AlreadyHaveAccountLink = (TextView) findViewById(R.id.already_have_account_link);
+    }
+
+    private void sendUserToLoginActivity() {
+        Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(loginIntent); // send user to login activity if already registered with an account
     }
 }
