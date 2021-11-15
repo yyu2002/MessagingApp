e5100d4db89b1654382cf2727ff8332d0bbc6c1f
Young Yu <youngyu2002@gmail.com>
2021-11-15 00:54:46 -0500

    created settings activity

---
+++ app/src/main/java/com/example/textmessagingapp/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.textmessagingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
